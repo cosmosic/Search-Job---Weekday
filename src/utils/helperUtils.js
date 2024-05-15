e79ba2ac71bfa82a3b91955b435b869824fa50e6
Ayushi Dokwal <115453513+cosmosic@users.noreply.github.com>
2024-05-15 21:25:58 +0530

    helping utils added

---
+++ src/utils/helperUtils.js
@@ -0,0 +1,8 @@
+export const capitalizeSentence = (sentence) => {
+    const words = sentence.split(" ");
+    const capitalizedWords = words.map((word) => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    });
+    return capitalizedWords.join(" ");
+  };
+  
