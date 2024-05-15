c5e3c1640a004d047f22f651413015303deb6092
Ayushi Dokwal <115453513+cosmosic@users.noreply.github.com>
2024-05-15 21:26:58 +0530

    Update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import * as React from "react";
+import { createTheme, ThemeProvider, CssBaseline } from "@mui/material";
+import JobsSearchPage from "./pages/JobsSearchPage";
+import "./App.css";
 
+const theme = createTheme({
+  typography: {
+    fontFamily: ["Lexend", "sans-serif"].join(","),
+  },
+  palette: {
+    primary: {
+      main: "#55EFC4",
+      light: "#55EFC4",
+      dark: "#55EFC4",
+    },
+  },
+});
 
 function App() {
   return (
-  <div>Hello World !</div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <JobsSearchPage />
+    </ThemeProvider>
   );
 }
 
